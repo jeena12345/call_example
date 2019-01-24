ce68c58cb61da766bf22c7c28f44b90041863d87
Inet <jeenajk@gmail.com>
2019-01-24 16:48:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/inet/call_example/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.inet.call_example;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+Button b1,b2;
+EditText no;
+    DatabaseReference rootRef,demoRef;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b2=findViewById(R.id.call);
+        b1=findViewById(R.id.fetch);
+        no=findViewById(R.id.no);
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        demoRef = rootRef.child("demo");
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+demoRef.setValue(no.getText().toString());
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                demoRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String value = dataSnapshot.getValue(String.class);
+                        Intent intent = new Intent(Intent.ACTION_DIAL);
+                        intent.setData(Uri.parse("tel:"+value));
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+            }
+        });
+    }
+}
